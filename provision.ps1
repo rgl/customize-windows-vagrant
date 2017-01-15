bdbce320ed53908894569cafa38efe5ef0cc15a3
Rui Lopes <rgl@ruilopes.com>
2017-01-15 08:14:31 +0000

    customize the taskbar buttons

--- provision.ps1
+++ provision.ps1
@@ -61,13 +61,52 @@ Set-ItemProperty -Path HKCU:Software\Microsoft\Windows\CurrentVersion\Explorer\A
 # show file extensions.
 Set-ItemProperty -Path HKCU:Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name HideFileExt -Value 0
 
+# cleanup the taskbar by removing the existing buttons and unpinning all applications; once the user logs on.
+# NB the shell executes these RunOnce commands about ~10s after the user logs on.
+[IO.File]::WriteAllText(
+    "$env:TEMP\ConfigureTaskbar.ps1",
+@'
+# unpin all applications.
+# NB this can only be done in a logged on session.
+$pinnedTaskbarPath = "$env:APPDATA\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar"
+(New-Object -Com Shell.Application).NameSpace($pinnedTaskbarPath).Items() `
+    | ForEach-Object {
+        $unpinVerb = $_.Verbs() | Where-Object { $_.Name -eq 'Unpin from tas&kbar' }
+        if ($unpinVerb) {
+            $unpinVerb.DoIt()
+        } else {
+            $shortcut = (New-Object -Com WScript.Shell).CreateShortcut($_.Path)
+            if (!$shortcut.TargetPath -and ($shortcut.IconLocation -eq '%windir%\explorer.exe,0')) {
+                Remove-Item -Force $_.Path
+            }
+        }
+    }
+Get-Item HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Taskband `
+    | Set-ItemProperty -Name Favorites -Value 0xff `
+    | Set-ItemProperty -Name FavoritesResolve -Value 0xff `
+    | Set-ItemProperty -Name FavoritesVersion -Value 3 `
+    | Set-ItemProperty -Name FavoritesChanges -Value 1 `
+    | Set-ItemProperty -Name FavoritesRemovedChanges -Value 1
+
+# hide the search button.
+Set-ItemProperty -Path HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name SearchboxTaskbarMode -Value 0
+
+# hide the task view button.
+Set-ItemProperty -Path HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name ShowTaskViewButton -Value 0
+
 # never combine the taskbar buttons.
-#
 # possibe values:
 #   0: always combine and hide labels (default)
 #   1: combine when taskbar is full
 #   2: never combine
 Set-ItemProperty -Path HKCU:Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name TaskbarGlomLevel -Value 2
+
+# restart explorer to apply the changed settings.
+(Get-Process explorer).Kill()
+'@)
+New-Item -Path HKCU:Software\Microsoft\Windows\CurrentVersion\RunOnce -Force `
+    | New-ItemProperty -Name ConfigureTaskbar -Value 'PowerShell -WindowStyle Hidden -File "%TEMP%\ConfigureTaskbar.ps1"' -PropertyType ExpandString `
+    | Out-Null
 
 # display full path in the title bar.
 New-Item -Path HKCU:Software\Microsoft\Windows\CurrentVersion\Explorer\CabinetState -Force `
